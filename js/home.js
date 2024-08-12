bd1ba4c92bccf6404108a067783e1410133cadc5
Sao-samnang <149239046+Sao-samnang@users.noreply.github.com>
2024-08-12 11:07:36 +0700

    commit --v1.03.0
    change link hire me

--- js/home.js
+++ js/home.js
@@ -6,6 +6,6 @@ $(document).ready(function(){
         $("body").css("height","200vh")
     },3000);
     $('.btn-home-top .bhire').click(function(){
-        window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=drive_link")
+        window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=sharing")
     })
 })
