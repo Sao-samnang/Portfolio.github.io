b73585c35a1073243527628c06774db9720fb873
Sao-samnang <149239046+Sao-samnang@users.noreply.github.com>
2024-08-12 10:27:59 +0700

    commit --v1.02
    fix loading page

--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+    $("body").css("height","100vh")
     setTimeout(()=>{
         $(".loading").remove();
         $(".contain-body").css("display","block")
+        $("body").css("height","200vh")
     },3000);
-    
     $('.btn-home-top .bhire').click(function(){
         window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=drive_link")
     })
