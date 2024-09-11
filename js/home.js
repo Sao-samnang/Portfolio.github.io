8c7581d2d427275b2f7cd40080369f2747a8706d
Sao-samnang <149239046+Sao-samnang@users.noreply.github.com>
2024-09-11 10:08:42 +0700

    home page design
    home design

--- js/home.js
+++ js/home.js
@@ -3,9 +3,27 @@ $(document).ready(function(){
     setTimeout(()=>{
         $(".loading").remove();
         $(".contain-body").css("display","block")
-        $("body").css("height","200vh")
+        $("body").css("height","auto")
     },3000);
     $('.btn-home-top .bhire').click(function(){
         window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=sharing")
     })
+    // Remove the transition class
+    const square = document.querySelector('.square');
+    square.classList.remove('square-transition');
+    
+    // Create the observer, same as before:
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          square.classList.add('square-transition');
+          return;
+        }
+    
+        square.classList.remove('square-transition');
+      });
+    });
+    
+    observer.observe(document.querySelector('.square-wrapper'));
+    squares.forEach((element) => observer.observe(element));
 })
