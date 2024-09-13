ccf6f1f19e708e2123909c48e5143dcbf30068a0
Sao-samnang <149239046+Sao-samnang@users.noreply.github.com>
2024-09-13 23:25:51 +0700

    home v0.0.5
    line contact

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $(".loading").remove();
         $(".contain-body").css("display","block")
         $("body").css("height","auto")
-    },2000);
+    },1000);
     $('.btn-home-top .bhire').click(function(){
         window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=sharing")
     })
