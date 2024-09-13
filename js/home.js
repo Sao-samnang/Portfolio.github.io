19a8ecae3d97f61093873c94932192246d604b53
Sao-samnang <149239046+Sao-samnang@users.noreply.github.com>
2024-09-13 22:50:02 +0700

    home -v0.0.4
    home page design

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $(".loading").remove();
         $(".contain-body").css("display","block")
         $("body").css("height","auto")
-    },3000);
+    },2000);
     $('.btn-home-top .bhire').click(function(){
         window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=sharing")
     })
