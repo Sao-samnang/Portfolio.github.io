26387fb4e3874af6bfecc5ab725c1361eb579241
Sao-samnang <149239046+Sao-samnang@users.noreply.github.com>
2024-12-11 15:36:09 +0700

    commite --v0.09
    fix menu

--- js/home.js
+++ js/home.js
@@ -8,26 +8,5 @@ $(document).ready(function(){
     $('.btn-home-top .bhire').click(function(){
         window.open("https://drive.google.com/file/d/1Pv2TDuAJEhHFMPecV8wqFVQmEKXswDax/view?usp=sharing")
     })
-    // Remove the transition class
-    const square = document.querySelector('.square');
-    square.classList.remove('square-transition');
     
-    // Create the observer, same as before:
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          square.classList.add('square-transition');
-          return;
-        }
-    
-        square.classList.remove('square-transition');
-      });
-    });
-    
-    observer.observe(document.querySelector('.square-wrapper'));
-    squares.forEach((element) => observer.observe(element));
-    // catch screen respone
-    // if (window.matchMedia('screen and (max-width: 410px)').matches) {
-    //   $(".nav-menu .brd-name").text("Portfolo🔥");
-    // }
 })
